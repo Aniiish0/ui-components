e837dae9efbbdaf67d473ce30c00bd2f2022702c
Subrat Kumar Pattanaik <subratpatnaik.email@gmail.com>
2024-08-20 22:03:58 +0530

    refactor: configuration changes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
-import "./index.css";
+export { default as Button } from "./components/Button";
+export { default as CheckBox } from "./components/CheckBox";
+export { default as CollapseCard } from "./components/CollapseCard";
+export { default as Form } from "./components/Form";
+export { default as Navbar } from "./components/Navbar";
+export { default as MapView } from "./components/MapView";
+export { default as Slider } from "./components/Slider";
+export { default as TextArea } from "./components/TextArea";
+export { default as TextInput } from "./components/TextInput";
 
-export * from "./components";
+export type {
+  ButtonProps,
+  Variants,
+  BootstrapVariants,
+} from "./components/Button/types";
+export type { CheckboxProps } from "./components/CheckBox/types";
+export type { CollapseCardProps } from "./components/CollapseCard/types";
+export type {
+  GenericFormProps,
+  FieldConfig,
+  FieldType,
+  ValidatorType,
+} from "./components/Form/types";
+export type {
+  NavbarProps,
+  ButtonProps as NavbarButtonProps,
+  NavbarItem,
+} from "./components/Navbar/types";
+export type { MapViewProps } from "./components/MapView/types";
+export type { SliderProps } from "./components/Slider/types";
+export type { TextAreaProps } from "./components/TextArea/types";
+export type { TextInputProps } from "./components/TextInput/types";
